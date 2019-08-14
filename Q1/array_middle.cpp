c88f82b16589f2e0d011d8df0eba106f3d7c933d
Eyong Kevin <tonyparkerkenz@gmail.com>
2019-08-14 15:57:15 +0100

    Change function to support standard array
    My function now takes an array and it length.

--- Q1/array_middle.cpp
+++ Q1/array_middle.cpp
@@ -1,46 +1,28 @@
 /* C++
-By  Eyong Kevin Enowanyo
+By  Eyong Kevin Enowanyo.
 
 An array with an odd number of elements is said to be centered if 
 all elements (except the middle one) are strictly greater than the value of 
 the middle element. Note that only arrays with an odd number of elements have 
 a middle element. Write a function that accepts an integer array and returns 1 
 if it is a centered array, otherwise it returns 0.
-
 */
-#include<iostream>
-#include<array>
 
-using namespace std;
-
-
-template <int n>
-int checkMiddleStatus(array<int, n>);
-
-// Import test file for testing.
-#include "test.cpp"
-
-int main(int argc, char* argv[])
-{
-    // Test function
-    testCheckMiddleStatus();
-
-  
-    return 0;
-
-
-}
-
-template <int n>
-int checkMiddleStatus(array<int, n> arr){ 
-    int getSize = arr.size();
-
-    if(getSize % 2 ==1 && getSize > 0){
-        int middIndex = getSize/2;
-        for(int i=0; i < getSize; i++){
+int checkMiddleStatus(int arr[], int len){ 
+    if(len % 2 ==1 && len > 0){
+        int middIndex = len/2;
+        for(int i=0; i < len; i++){
             if(i != middIndex && arr[middIndex] >= arr[i]) return 0;
         }
         return 1;
     }
+    return 0;
+}
+#include "test.cpp"
+
+int main() 
+{
+    testCheckMiddleStatus();
+
     return 0;
 }
--- Q1/test.cpp
+++ Q1/test.cpp
@@ -1,45 +1,45 @@
-
 #include <assert.h> 
 #include<iomanip>
+#include<iostream>
+
+using namespace std;
 
 void testCheckMiddleStatus()
 {
-    array<int, 5> arr1={1, 2, 3, 4, 5};
-        array<int, 5> arr2={3, 2, 1, 4, 5};
-        array<int, 5> arr3={3, 2, 1, 4, 1};
-        array<int, 4> arr4={1, 2, 3, 4};
-        array<int, 0> arr5={};
-        array<int, 1> arr6={10};
+        int arr1[]={1, 2, 3, 4, 5};
+        int arr2[]={3, 2, 1, 4, 5};
+        int arr3[]={3, 2, 1, 4, 1};
+        int arr4[]={1, 2, 3, 4};
+        int arr5[]={};
+        int arr6[]={10};
 
 
         int answer;
-        answer = checkMiddleStatus<arr1.size()>(arr1);
+        answer = checkMiddleStatus(arr1, sizeof(arr1)/sizeof(arr1[0]));
         assert(answer == 0);
         cout<<"{1, 2, 3, 4, 5}: "<<answer<<endl;
         
-
-        answer = checkMiddleStatus<arr2.size()>(arr2);
+        answer = checkMiddleStatus(arr2, sizeof(arr2)/sizeof(arr2[0]));
         assert(answer == 1);
         cout<<"{3,2,1,4,5}"<<std::setw(6)<<": "<<answer<<endl;
 
-        answer = checkMiddleStatus<arr3.size()>(arr3);
+        answer = checkMiddleStatus(arr3, sizeof(arr3)/sizeof(arr3[0]));
         assert(answer == 0);
         cout<<"{3, 2, 1, 4, 1}: "<<answer<<endl;
 
-        answer = checkMiddleStatus<arr5.size()>(arr5);
+        answer = checkMiddleStatus(arr4, sizeof(arr4)/sizeof(arr4[0]));
         assert(answer == 0);
         cout<<"{1, 2, 3, 4}"<<std::setw(5)<<": "<<answer<<endl;
 
-        answer = checkMiddleStatus<arr4.size()>(arr4);
+        answer = checkMiddleStatus(arr5, sizeof(arr5)/sizeof(arr5[0]));
         assert(answer == 0);
         cout<<"{}"<<std::setw(15)<<": "<<answer<<endl;
 
-        answer = checkMiddleStatus<arr6.size()>(arr6);
+        answer = checkMiddleStatus(arr6, sizeof(arr6)/sizeof(arr6[0]));
         assert(answer == 1);
         cout<<"{10}"<<std::setw(13)<<": "<<answer<<endl;
+        
         
         cout<<"-----------------\nPassed"<<endl;
 
 }
-
-
